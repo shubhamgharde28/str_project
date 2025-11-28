3974352aecd2174adbb33617022f6943533fd409
shubham <shubhamgharde28@gmail.com>
2025-11-28 22:17:10 +0530

    mysql added

--- str_project/settings.py
+++ str_project/settings.py
@@ -62,18 +62,18 @@ WSGI_APPLICATION = 'str_project.wsgi.application'
 
 
 DATABASES = {
-    'default': {
+    'SQLT': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    # 'Mysql': {
-    #     'ENGINE': config('DB_ENGINE', default='django.db.backends.sqlite3'),
-    #     'NAME': config('DB_NAME', default=BASE_DIR / 'db.sqlite3'),
-    #     'USER': config('DB_USER', default=''),
-    #     'PASSWORD': config('DB_PASSWORD', default=''),
-    #     'HOST': config('DB_HOST', default='localhost'),
-    #     'PORT': config('DB_PORT', default='3306'),
-    # }
+    'default': {
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
 }
 
 
